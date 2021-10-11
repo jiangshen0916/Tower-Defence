72c19606f0c4dc297c2dccfc68ab42004d58b3da
jiangshen <3045569907@qq.com>
2021-10-11 10:31:16 +0800

    塔防

---
+++ Tower Defence/Assets/script/Loading.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/**
+ * @author ： jiangshen
+ * @function : 场景跳转
+ */
+
+public class Loading : MonoBehaviour
+{
+  public  Button button;
+    void Start()
+    {
+        // 利用匿名委托跳转场景
+       button.onClick.AddListener(delegate() { SceneManager.LoadScene("Game"); });
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
